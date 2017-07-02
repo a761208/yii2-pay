b770def0d62040425655763c34d5dec466acf561
侯伟 <a761208@gmail.com>
2017-07-02 17:25:50 +0800

    显示支付内容页面触发方法单独出来

--- assets/paychoice.js
+++ assets/paychoice.js
@@ -33,50 +33,66 @@ jQuery(function($) {
 
                 if (options['popup'] !== false) {
                     // 通过弹出小窗口支付
-                    var payChoicePopup = $container.data('payChoicePopup');
-                    if (payChoicePopup) {
-                        payChoicePopup.close();
-                    }
-
-                    var popupOptions = $.extend({}, options.popup); // clone
-                    var localPopupWidth = this.getAttribute('data-popup-width');
-                    if (localPopupWidth) {
-                        popupOptions.width = localPopupWidth;
-                    }
-                    var localPopupHeight = this.getAttribute('data-popup-height');
-                    if (localPopupWidth) {
-                        popupOptions.height = localPopupHeight;
-                    }
-                    popupOptions.left = (window.screen.width - popupOptions.width) / 2;
-                    popupOptions.top = (window.screen.height - popupOptions.height) / 2;
-                    var popupFeatureParts = [];
-                    for (var propName in popupOptions) {
-                        if (popupOptions.hasOwnProperty(propName)) {
-                            popupFeatureParts.push(propName + '=' + popupOptions[propName]);
-                        }
-                    }
-
-                    var popupFeature = popupFeatureParts.join(',');
-                    payChoicePopup = window.open(url, 'yii_pay_choice', popupFeature);
-                    payChoicePopup.focus();
-                    $container.data('payChoicePopup', payChoicePopup);
+                    payPopup($container, options, this, url);
                 } else {
-                    var iframe = '<iframe id="yii_pay_choice" src="' + url + '" style="border:none; position:absolute; left:0; top:0; width:100%; height:' + $(document).height() + 'px; background:#FFF;"></iframe>';
-                    $container.after(iframe);
-                    // 通过嵌入iframe支付
+                    payIframe($container, url);
                 }
             });
         });
     };
 });
 /**
+ * 弹出窗口支付
+ * @param $container
+ * @param options
+ * @param obj
+ * @param url
+ */
+function payPopup($container, options, obj, url) {
+    var payChoicePopup = $container.data('payChoicePopup');
+    if (payChoicePopup) {
+        payChoicePopup.close();
+    }
+
+    var popupOptions = $.extend({}, options.popup); // clone
+    var localPopupWidth = obj.getAttribute('data-popup-width');
+    if (localPopupWidth) {
+        popupOptions.width = localPopupWidth;
+    }
+    var localPopupHeight = obj.getAttribute('data-popup-height');
+    if (localPopupWidth) {
+        popupOptions.height = localPopupHeight;
+    }
+    popupOptions.left = (window.screen.width - popupOptions.width) / 2;
+    popupOptions.top = (window.screen.height - popupOptions.height) / 2;
+    var popupFeatureParts = [];
+    for (var propName in popupOptions) {
+        if (popupOptions.hasOwnProperty(propName)) {
+            popupFeatureParts.push(propName + '=' + popupOptions[propName]);
+        }
+    }
+
+    var popupFeature = popupFeatureParts.join(',');
+    payChoicePopup = window.open(url, 'yii_pay_choice', popupFeature);
+    payChoicePopup.focus();
+    $container.data('payChoicePopup', payChoicePopup);
+}
+/**
+ * 通过嵌入iframe支付
+ * @param $container
+ * @param url
+ */
+function payIframe($container, url) {
+    var iframe = '<iframe id="yii_pay_choice" src="' + url + '" style="border:none; position:absolute; left:0; top:0; width:100%; height:' + $(document).height() + 'px; background:#FFF;"></iframe>';
+    $container.append(iframe);
+}
+/**
  * 检查支付结果并回调
  * @param url string 检查结果的地址
  * @param params 提交参数
  * @returns
  */
-function checkPayResult(url, params)
-{
+function checkPayResult(url, params) {
     $.getJSON(url, params, function(json) {
         if (json['result'] === 'success') { // 返回结果正常
             if (json['pay_result'] === 'success') { // 支付成功
